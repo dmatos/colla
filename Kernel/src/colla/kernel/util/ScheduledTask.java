486df754e5a5c5ca68b7e96eebc431cfa92d45de
Diogo Matos <dmatos88@gmail.com>
2013-09-06 14:14:26 -0300

    adaptações à integração com JCL Pacu

---
+++ Kernel/src/colla/kernel/util/ScheduledTask.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package colla.kernel.util;
+
+import colla.kernel.api.GenericResource;
+import colla.kernel.api.CollAMessage;
+import java.util.TimerTask;
+
+/**
+ * Wait the call from a Timer to execute a Task.
+ * 
+ * @author dmatos
+ */
+public class ScheduledTask extends TimerTask {
+
+    public ScheduledTask(final CollAMessage message, final GenericResource resource) {
+        this.taskMessage = message;
+        this.resource = resource;
+    }
+
+    @Override
+    public void run() {
+        this.resource.putRegister(this.taskMessage);
+    }
+    
+    private final CollAMessage taskMessage;
+    private final GenericResource resource;
+}
